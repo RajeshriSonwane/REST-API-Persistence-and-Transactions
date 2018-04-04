f968985a256d0caff654f8947be91587847318fb
unknown <rajashreesonawane26@gmail.com>
2018-04-04 14:32:01 -0700

    Initial commit

---
+++ src/main/java/edu/sjsu/cmpe275/lab2/Plane/Plane.java
@@ -0,0 +1,40 @@
+package edu.sjsu.cmpe275.lab2.Plane;
+
+public class Plane {
+    private int capacity;
+    private String model;
+    private String manufacturer;
+    private int year;
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
